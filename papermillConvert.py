318365954f1f7d011224129413fef8374c65fbe0
Nick <59539266+nick-gould@users.noreply.github.com>
2026-03-13 14:06:27 -0700

    Add files via upload

---
+++ papermillConvert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: nickgould
+"""
+
+import subprocess
+
+from datetime import datetime
+
+def Today():
+    return datetime.now().strftime("%Y%m%d")
+
+def ThisTime():
+    return datetime.now().strftime("%Y%m%d_%H%M%S")
+
+
+myNotebook = 'Input-MonteCarloPi.ipynb'
+# myOutput = myNotebook.split('.')[0] + '_OUTPUT.' + myNotebook.split('.')[1]
+myOutput = 'output/' + 'Output-MonteCarloPi' + ThisTime() + '.ipynb'
+
+
+bashCommand = f'papermill {myNotebook} {myOutput}'
+# bashCommand = f'jupyter nbconvert --to html {myOutput} --output {myFile}'
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+
